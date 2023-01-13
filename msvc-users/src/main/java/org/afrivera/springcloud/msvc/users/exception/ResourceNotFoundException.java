f46c03d9f22601e10336cb716acbedfd7301c540
afrivera <andresrfelip@hotmail.com>
2023-01-13 09:25:26 -0500

    add class to catch ResourceNotFoundException

---
+++ msvc-users/src/main/java/org/afrivera/springcloud/msvc/users/exception/ResourceNotFoundException.java
@@ -0,0 +1,59 @@
+package org.afrivera.springcloud.msvc.users.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    private String resourceName;
+    private String fieldName;
+    private long fieldValue;
+    private String nameValue;
+
+    public ResourceNotFoundException(String message, String resourceName, String fieldName, long fieldValue) {
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public ResourceNotFoundException(String message, String resourceName, String fieldName, String  nameValue) {
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, nameValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.nameValue = nameValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public long getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(long fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    public String getNameValue() {
+        return nameValue;
+    }
+
+    public void setNameValue(String nameValue) {
+        this.nameValue = nameValue;
+    }
+}
