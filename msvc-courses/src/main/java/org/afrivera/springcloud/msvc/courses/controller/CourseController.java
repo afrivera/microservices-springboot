68b0e9722890d3833dc7a777d9b20992d4f82761
afrivera <andresrfelip@hotmail.com>
2023-01-16 14:34:51 -0500

    add CourseController, config endpoints: FindAll, FindById and save, config params on application.properties to connect to db

---
+++ msvc-courses/src/main/java/org/afrivera/springcloud/msvc/courses/controller/CourseController.java
@@ -0,0 +1,36 @@
+package org.afrivera.springcloud.msvc.courses.controller;
+
+import org.afrivera.springcloud.msvc.courses.dto.CourseRequestDto;
+import org.afrivera.springcloud.msvc.courses.dto.CourseResponseDto;
+import org.afrivera.springcloud.msvc.courses.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponseDto>> findAll(){
+        return new ResponseEntity<>(courseService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseResponseDto> findById(@PathVariable Long id){
+        return new ResponseEntity<>(courseService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseResponseDto> save(@Valid @RequestBody CourseRequestDto courseRequestDto){
+        return new ResponseEntity<>(courseService.save(courseRequestDto), HttpStatus.CREATED);
+    }
+}
