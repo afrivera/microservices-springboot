e091505434e846c7d3beb83073d50fc0e7458d39
afrivera <andresrfelip@hotmail.com>
2023-01-17 09:41:26 -0500

    add ResourceNotFoundException Class to catch exceptions

--- msvc-courses/src/main/java/org/afrivera/springcloud/msvc/courses/exception/ResourceNotFoundException.java
+++ msvc-courses/src/main/java/org/afrivera/springcloud/msvc/courses/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package org.afrivera.springcloud.msvc.courses.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
     private String resourceName;
