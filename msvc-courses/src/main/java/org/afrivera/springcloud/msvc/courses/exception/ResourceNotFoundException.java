a8da21b229b5fd2165584d409133104759c522bb
afrivera <andresrfelip@hotmail.com>
2023-01-17 09:39:33 -0500

    add ResourceNotFoundException Class to catch exceptions

---
+++ msvc-courses/src/main/java/org/afrivera/springcloud/msvc/courses/exception/ResourceNotFoundException.java
@@ -0,0 +1,48 @@
+package org.afrivera.springcloud.msvc.courses.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    private String resourceName;
+    private String fieldName;
+    private long fieldValue;
+    private String nameValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+        super(String.format("%s not found with %s : %S", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public long getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(long fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    public String getNameValue() {
+        return nameValue;
+    }
+
+    public void setNameValue(String nameValue) {
+        this.nameValue = nameValue;
+    }
+}
